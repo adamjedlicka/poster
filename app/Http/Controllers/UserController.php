e3037b32befb4c63eb7ca049a4e59cb6b5e9c0ad
Adam Jedlička <adajedlicka@gmail.com>
2019-03-12 09:58:28 +0100

    Users profile posts order changed to descending

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     {
         return view('users.detail', [
             'user' => $user,
-            'posts' => $user->posts,
+            'posts' => $user->posts()->orderBy('created_at', 'desc')->get(),
         ]);
     }
 
