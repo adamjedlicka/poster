c84e146ab3d738f4be14dd473745e04becda3845
Adam Jedlička <adajedlicka@gmail.com>
2019-04-17 19:13:40 +0200

    User detail has list of followed topics

--- resources/views/users/detail.blade.php
+++ resources/views/users/detail.blade.php
@@ -2,45 +2,62 @@
 @section('content')
 
 <div class="ui segment grid">
+
     <div class="ui right very close rail">
         @if(Auth::id() === $user->getKey())
-            <div class="ui vertical menu">
-                @can('update', $user)
-                    <a href="{{ route('users.edit', $user) }}" class="item">Edit profile</a>
-                @endcan
+        <div class="ui vertical menu">
+            @can('update', $user)
+            <a href="{{ route('users.edit', $user) }}" class="item">Edit profile</a>
+            @endcan
 
-                <post-button url="{{ route('logout') }}">Logout</post-button>
-            </div>
+            <post-button url="{{ route('logout') }}">Logout</post-button>
+        </div>
         @endif
 
         @auth @if(Auth::id() !== $user->getKey())
-            <div class="ui vertical menu">
-                <follow-button url="{{ route('follow.user', $user) }}" follows="{{ Auth::user()->follows($user) }}"></follow-button>
-            </div>
+        <div class="ui vertical menu">
+            <follow-button url="{{ route('follow.user', $user) }}" follows="{{ Auth::user()->follows($user) }}"></follow-button>
+        </div>
         @endif @endauth
     </div>
 
     <div class="ui row">
         <div class="two wide column">
-            <img class="ui image" src="{{ $user->avatar }}">
+            <img class="ui image" src="{{ $user->avatar }}" alt="profile picture">
         </div>
 
         <div class="fourteen wide column">
-            <h1 class="ui header">
+            <div class="ui header">
                 {{ $user->first_name }} {{ $user->last_name }}
                 <div class="sub header">
                     {{ '@' }}{{ $user->handle }}
                 </div>
-            </h1>
+            </div>
 
             <div class="ui label">
                 Followers
-                <div class="detail">{{ $user->followerCount }}</div>
+                <div class="detail">{{ $user->followers()->count() }}</div>
+            </div>
+
+            <div class="ui label">
+                Followed users
+                <div class="detail">{{ $user->follows()->count() }}</div>
             </div>
 
             <div class="ui label">
                 Posts
-                <div class="detail">{{ $user->postCount }}</div>
+                <div class="detail">{{ $user->posts()->count() }}</div>
+            </div>
+
+            <div class="ui row">
+                <div class="ui column">
+                    Followd topics:
+                    @foreach($user->followsTopic as $topic)
+                    <a href="{{ route('topics.show', $topic) }}">
+                        {{ $topic->name }}
+                    </a>
+                    @endforeach
+                </div>
             </div>
         </div>
 
@@ -49,7 +66,7 @@
 
 <div class="ui segment">
     @include('posts._index', [
-        'title' => $user->first_name . '\'s posts',
+    'title' => $user->first_name . '\'s posts',
     ])
 </div>
 @endsection
